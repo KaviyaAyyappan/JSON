8696b524c4b8b7d686141b034cd8c3c8a7986d2d
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-09-07 11:32:48 +0200

    first commit

---
+++ JsonCreate/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonCreate
+{
+    class Employee
+    {
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public int EmployeeId { get; set; }
+        //public string Designation { get; set; }
+
+        //Assigning value directly to variable
+        public string FirstName = "Deva";
+        public string LastName = "Sumitha";
+        public int EmployeeId = 2657892;
+        public string Designation = "Manager";
+    }
+}
---
+++ JsonCreate/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            string JSONresult = JsonConvert.SerializeObject(emp);
+            string path = @"C:\Users\kaviya\source\repos\JsonCreate\employee.json";
+            if(File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+
+        }
+    }
+}

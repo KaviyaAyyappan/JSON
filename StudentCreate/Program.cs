958b00c884e0aef40f9904cd1e27ffd4c056ad56
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-09-07 13:34:53 +0200

    Created Student JSON.

---
+++ StudentCreate/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+            string JSONresult = JsonConvert.SerializeObject(stud);
+            string path = @"C:\Users\kaviya\source\repos\JsonCreate\StudentCreate\Student.json";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+            else if (!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ StudentCreate/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCreate
+{
+    class Student
+    {
+        public string Name = "Deva";
+        public int Class = 10;
+        public int RollNo = 2547862;
+        public string[] Subject = { "English", "Math", "Science" };
+    }
+}

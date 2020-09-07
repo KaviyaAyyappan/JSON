960ed74c96da5f6f2c49623de8afaf5a5c4b7cf5
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-09-07 14:27:14 +0200

    Read the Json file into C# objects.

---
+++ JsonObj/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace JsonObj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = @"{""Name"":""Deva"",""Class"":10,""RollNo"":5489625,""Subjects"":[""Math"",""Science""]}";
+
+            var Students = new JavaScriptSerializer().Deserialize<Student>(students);
+
+           // string name = Students.Name;
+           
+            
+                Console.WriteLine(Students.Name+" " + Students.Class+" " + Students.RollNo +" "+ Students.Subjects[0]);
+                Console.ReadLine();
+            
+            
+
+            
+        }
+    }
+}
---
+++ JsonObj/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonObj
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public int RollNo { get; set; }
+        public string[] Subjects { get; set; }
+
+    }
+}
